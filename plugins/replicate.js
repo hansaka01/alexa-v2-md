b14225aeebaad925da034f55642e32ef1c67f870
Hansaka01 <rasanjanahansaka@gmail.com>
2023-04-04 18:51:07 +0530

    upload all files

---
+++ plugins/replicate.js
@@ -0,0 +1,13 @@
+
+const Replicate = require('replicate')
+
+const replicate = new Replicate({
+  auth: "e1b06947a4e66ac6f9d64d2175a8442db8d312e1",
+});
+
+const model = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
+const input = { prompt: "an astronaut riding a horse on mars, hd, dramatic lighting, detailed" };
+const output = await replicate.run(model, { input });
+
+console.log(output);
+// ['https://replicate.com/api/models/stability-ai/stable-diffusion/files/50fcac81-865d-499e-81ac-49de0cb79264/out-0.png']
